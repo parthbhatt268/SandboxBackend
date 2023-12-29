977d846e063ed95bea11e06dd46e353d182720af
parthbhatt268 <parthbhatt268@gmail.com>
2023-12-29 10:23:35 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express")
+//const cors =  require("cors")
+var bodyParser = require('body-parser')
+//const router = require("./src/Router/index")
+//const  ErrorHandlerMw = require("./src/Middleware/error-handler.mw")
+//const jwt = require('jsonwebtoken');
+
+
+const app = express()
+
+//app.use(cors())
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,HEAD,OPTIONS');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
+    next();
+ });
+app.use(express.json({ limit: "50mb" }))
+app.use(express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }))
+
+app.use(bodyParser.json())
+//app.use("/api/v1/rapidBasket",router)
+
+
+//app.use(ErrorHandlerMw)
+
+app.get('*', function(req, res){
+    res.status(404).send("Something went Wrong. Please refresh!!!");
+    });
+
+module.exports = app
+
+//router 
+//middleware
+//main api code - controller
---
+++ index.js
@@ -0,0 +1,40 @@
+//0. npm init
+//1. cretae server
+//2. connect with mongo
+//3. 
+const mongoose = require("mongoose")
+const dotnev = require("dotenv");
+//const jwt = require('jsonwebtoken');
+const app = require("./app")
+
+dotnev.config({ path: "./.env" });
+//const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+const DB = process.env.DATABASE.replace(
+  "<password>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB,  { useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => console.log("DB connection successful!"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+const port = process.env.port;
+
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
+
+process.on("uncaughtException", (err) => {
+  console.log(err.name, err.message);
+  console.log("Uncaght Exception. Shutting Down....");
+  server.close(() => {
+    process.exit(1); // to properly end the server
+  });
+});
+
+
+//module.exports = stripe
